b7ea89b9b016b3e8f7cdbd057ba35f7cb0d9fe1f
QSmally <github@qsmally.org>
2024-10-04 15:09:48 +0200

    Initial Commit

---
+++ System-Audio/System_AudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  System_AudioApp.swift
+//  System-Audio
+//
+//  Created by Joey Smalen on 04/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct System_AudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
